e23ed573a736c3a05c64e93daf47be8678adce72
Alexander Boehm <akboehm13@outlook.com>
2023-08-08 15:31:54 -0400

    [vue]

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -5,6 +5,7 @@ import Login from '../views/Login.vue'
 import Logout from '../views/Logout.vue'
 import Register from '../views/Register.vue'
 import store from '../store/index'
+import GlobalList from '@/GlobalList.vue';
 
 Vue.use(Router)
 
@@ -49,6 +50,14 @@ const router = new Router({
       path: "/register",
       name: "register",
       component: Register,
+      meta: {
+        requiresAuth: false
+      }
+    },
+    {
+      path: "/global_list",
+      name: "global_list",
+      component: GlobalList,
       meta: {
         requiresAuth: false
       }
