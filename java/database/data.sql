3c9d2e13164e95d714d9c0999f253bba79be9bd2
Eric Blanchet <ericblanchet1997@gmail.com>
2023-08-08 13:22:52 -0400

    Updated Database: Working with fake data

--- java/database/data.sql
+++ java/database/data.sql
@@ -1,6 +1,64 @@
 BEGIN TRANSACTION;
 
-INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
-INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
+-- INSERT FOR USER TABLE
+
+INSERT INTO "user" (username,password_hash,role,email) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER','randomuseremail@gmail.com');
+INSERT INTO "user" (username,password_hash,role,email) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN','randomadminuser@gmail.com');
+
+-- INSERT FOR SONG TABLE (FAKE SONGS) LIST OF GENRES TO USE (POP, HIP-HOP, ROCK, JAZZ, DANCE, HEAVY METAL, ALTERNATIVE, INSTRUMENTAL, INDIE)
+
+INSERT INTO "song" (title, artist, genre, duration) VALUES ('Dance All Night', 'Sunny Beats', 'Pop', '3:45');
+INSERT INTO "song" (title, artist, genre, duration) VALUES ('Groove Machine', 'MC Rhymes', 'Hip-Hop', '4:20');
+INSERT INTO "song" (title, artist, genre, duration) VALUES ('Midnight Blues', 'Smooth Jazz Ensemble', 'Jazz', '5:10');
+INSERT INTO "song" (title, artist, genre, duration) VALUES ('Rock Revolution', 'Power Chords', 'Rock', '3:30');
+INSERT INTO "song" (title, artist, genre, duration) VALUES ('Echoes of Tomorrow', 'Alternate Realities', 'Alternative', '4:45');
+INSERT INTO "song" (title, artist, genre, duration) VALUES ('Indie Visions', 'Lonely Wanderer', 'Indie', '3:55');
+INSERT INTO "song" (title, artist, genre, duration) VALUES ('Disco Fever', 'Groovy Tunes', 'Disco', '4:00');
+INSERT INTO "song" (title, artist, genre, duration) VALUES ('Eternal Melody', 'Harmonic Waves', 'Instrumental', '2:30');
+INSERT INTO "song" (title, artist, genre, duration) VALUES ('Dancefloor Anthem', 'Electric Pulse', 'Dance', '3:15');
+INSERT INTO "song" (title, artist, genre, duration) VALUES ('Metal Mayhem', 'Steel Storm', 'Heavy Metal', '4:40');
+INSERT INTO "song" (title, artist, genre, duration) VALUES ('Sunset Serenade', 'Melody Lane', 'Pop', '4:10');
+INSERT INTO "song" (title, artist, genre, duration) VALUES ('Urban Legends', 'City Stories', 'Hip-Hop', '3:20');
+INSERT INTO "song" (title, artist, genre, duration) VALUES ('Jazzy Nights', 'Saxophone Solace', 'Jazz', '4:50');
+INSERT INTO "song" (title, artist, genre, duration) VALUES ('Rock & Roll Dreams', 'Riff Masters', 'Rock', '3:40');
+INSERT INTO "song" (title, artist, genre, duration) VALUES ('Shifting Realities', 'Parallel Universe', 'Alternative', '5:05');
+INSERT INTO "song" (title, artist, genre, duration) VALUES ('Indie Heart', 'Emotional Echoes', 'Indie', '3:30');
+INSERT INTO "song" (title, artist, genre, duration) VALUES ('Saturday Night Fever', 'Disco Dynamite', 'Disco', '4:25');
+INSERT INTO "song" (title, artist, genre, duration) VALUES ('Enchanted Piano', 'Keys of Serenity', 'Instrumental', '3:20');
+INSERT INTO "song" (title, artist, genre, duration) VALUES ('Electro Groove', 'Synth Magic', 'Dance', '4:15');
+INSERT INTO "song" (title, artist, genre, duration) VALUES ('Metallic Thunder', 'Iron Titans', 'Heavy Metal', '5:00');
+INSERT INTO "song" (title, artist, genre, duration) VALUES ('Summer Breeze', 'Beach Vibes', 'Pop', '3:30');
+INSERT INTO "song" (title, artist, genre, duration) VALUES ('City Lights', 'Urban Flow', 'Hip-Hop', '4:10');
+INSERT INTO "song" (title, artist, genre, duration) VALUES ('Saxophone Serenade', 'Smooth Jazz Notes', 'Jazz', '5:20');
+INSERT INTO "song" (title, artist, genre, duration) VALUES ('Wildfire Rock', 'Rebel Sound', 'Rock', '3:50');
+INSERT INTO "song" (title, artist, genre, duration) VALUES ('Echoes of Rebellion', 'Alternative Echo', 'Alternative', '4:25');
+INSERT INTO "song" (title, artist, genre, duration) VALUES ('Indie Echoes', 'Whispering Echoes', 'Indie', '3:45');
+INSERT INTO "song" (title, artist, genre, duration) VALUES ('Neon Disco', 'Funky Grooves', 'Disco', '4:30');
+INSERT INTO "song" (title, artist, genre, duration) VALUES ('Piano Dreams', 'Key Harmony', 'Instrumental', '2:50');
+INSERT INTO "song" (title, artist, genre, duration) VALUES ('Electro Dancefloor', 'Synthetic Beats', 'Dance', '3:15');
+INSERT INTO "song" (title, artist, genre, duration) VALUES ('Metal Mania', 'Steel Symphony', 'Heavy Metal', '4:40');
+INSERT INTO "song" (title, artist, genre, duration) VALUES ('Sunset Groove', 'Harmonic Skies', 'Pop', '4:15');
+INSERT INTO "song" (title, artist, genre, duration) VALUES ('Street Poetry', 'Urban Rhymes', 'Hip-Hop', '3:30');
+INSERT INTO "song" (title, artist, genre, duration) VALUES ('Jazz Noir', 'Midnight Jazz Ensemble', 'Jazz', '4:40');
+INSERT INTO "song" (title, artist, genre, duration) VALUES ('Rock Fusion', 'Electrified Sound', 'Rock', '3:55');
+INSERT INTO "song" (title, artist, genre, duration) VALUES ('Parallel Realities', 'Alternate Dimension', 'Alternative', '4:50');
+INSERT INTO "song" (title, artist, genre, duration) VALUES ('Indie Odyssey', 'Wandering Souls', 'Indie', '3:40');
+INSERT INTO "song" (title, artist, genre, duration) VALUES ('Glowing Disco', 'Luminous Beats', 'Disco', '4:25');
+INSERT INTO "song" (title, artist, genre, duration) VALUES ('Ethereal Keys', 'Heavenly Harmony', 'Instrumental', '3:20');
+INSERT INTO "song" (title, artist, genre, duration) VALUES ('Electro Euphoria', 'Digital Dreams', 'Dance', '4:15');
+INSERT INTO "song" (title, artist, genre, duration) VALUES ('Raging Metal', 'Thunderstorm', 'Heavy Metal', '5:10');
+
+INSERT INTO "event" (name, dj_id, host_id, date, description, start_time, end_time) VALUES ('TechElevator Graduation', 2, 1, '12/18/2023', 'This marks the end of the 4 month boot camp! Congrats!', '12:00:00','14:00:00');
+
+INSERT INTO "playlist" (event_id, name) VALUES (1, 'TEchnoElevator');
+
+INSERT INTO "playlist_song" (playlist_id, song_id) VALUES (1, 5);
+INSERT INTO "playlist_song" (playlist_id, song_id) VALUES (1, 7);
+INSERT INTO "playlist_song" (playlist_id, song_id) VALUES (1, 10);
+INSERT INTO "playlist_song" (playlist_id, song_id) VALUES (1, 19);
+INSERT INTO "playlist_song" (playlist_id, song_id) VALUES (1, 22);
+INSERT INTO "playlist_song" (playlist_id, song_id) VALUES (1, 23);
+INSERT INTO "playlist_song" (playlist_id, song_id) VALUES(1, 35);
+INSERT INTO "playlist_song" (playlist_id, song_id) VALUES (1, 39);
 
 COMMIT TRANSACTION;
--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,13 +1,55 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS "user";
+DROP TABLE IF EXISTS "song";
+DROP TABLE IF EXISTS "event";
+DROP TABLE IF EXISTS "playlist";
+DROP TABLE IF EXISTS "playlist_song";
 
-CREATE TABLE users (
-	user_id SERIAL,
-	username varchar(50) NOT NULL UNIQUE,
-	password_hash varchar(200) NOT NULL,
-	role varchar(50) NOT NULL,
-	CONSTRAINT PK_user PRIMARY KEY (user_id)
+CREATE TABLE "user" (
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(30) UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL,
+    email VARCHAR(50) UNIQUE NOT NULL,
+    role VARCHAR(10) NOT NULL
 );
+
+CREATE TABLE "song" (
+    song_id SERIAL PRIMARY KEY,
+    title VARCHAR(75) NOT NULL,
+    artist VARCHAR(75) NOT NULL,
+    genre VARCHAR(25) NOT NULL,
+    duration INTERVAL NOT NULL
+);
+
+CREATE TABLE "event" (
+    event_id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    dj_id INT NOT NULL,
+    host_id INT NOT NULL,
+    date DATE NOT NULL,
+    description TEXT,
+    start_time TIME NOT NULL,
+    end_time TIME NOT NULL
+);
+
+CREATE TABLE "playlist" (
+    playlist_id SERIAL PRIMARY KEY,
+    event_id INT NOT NULL,
+    name VARCHAR(75) NOT NULL,
+    CONSTRAINT playlist_fk_event FOREIGN KEY (event_id) REFERENCES "event"(event_id)
+);
+
+CREATE TABLE "playlist_song" (
+    playlist_id INT NOT NULL,
+    song_id INT NOT NULL,
+    CONSTRAINT playlist_song_fk_playlist FOREIGN KEY (playlist_id) REFERENCES "playlist"(playlist_id),
+    CONSTRAINT playlist_song_fk_song FOREIGN KEY (song_id) REFERENCES "song"(song_id),
+    CONSTRAINT playlist_song_pk PRIMARY KEY (playlist_id, song_id)
+);
+
+ALTER TABLE "event"
+    ADD CONSTRAINT dj_fk FOREIGN KEY (dj_id) REFERENCES "user"(user_id),
+    ADD CONSTRAINT host_fk FOREIGN KEY (host_id) REFERENCES "user"(user_id);
 
 COMMIT TRANSACTION;
