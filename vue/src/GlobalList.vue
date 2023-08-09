335a8a271ddfff94c9c0b8ec7584ad7f41719c7f
Alex Schuchter <anschuchter@gmail.com>
2023-08-09 13:02:34 -0400

    bug fixes and axios added

--- vue/src/GlobalList.vue
+++ vue/src/GlobalList.vue
@@ -1,47 +1,51 @@
 <template>
-
-<div>
+  <div>
     <div class="input">
-        <h2>Global Music List</h2>
-        <input
-        type="text"
-        v-model="searchQuery"
-        @input="handleSearch"
-        placeholder="Search songs..."
-        />
-        <button @click="showAddForm = true">+ Add Song</button>
+      <h2>Global Music List</h2>
+      <input type="text" v-model="searchQuery" placeholder="Search songs..." />
+      <button @click="showForm()">+ Add Song</button>
     </div>
 
     <div class="add-song-form" v-if="showAddForm">
       <form @submit.prevent="addNewSong">
-          <input type="text" v-model="editingSong.name" placeholder="Name" />
-          <input type="text" v-model="editingSong.artist" placeholder="Artist" />
-          <input type="text" v-model="editingSong.genre" placeholder="Genre" />
-          <input type="text" v-model="editingSong.duration" placeholder="Duration" />
-          <input type="text" v-model="editingSong.coverArt" placeholder="Cover Art URL" />
-          <button type="submit">{{ editingSongIndex === -1 ? 'Add' : 'Update' }}</button>
-          <button type="button" @click="cancelEdit">Cancel</button>
+        <input type="text" v-model="editingSong.title" placeholder="Name" />
+        <input type="text" v-model="editingSong.artist" placeholder="Artist" />
+        <input type="text" v-model="editingSong.genre" placeholder="Genre" />
+        <input
+          type="text"
+          v-model="editingSong.duration"
+          placeholder="Duration"
+        />
+        <input
+          type="text"
+          v-model="editingSong.coverArt"
+          placeholder="Cover Art URL"
+        />
+        <button type="submit">
+          {{ editingSongIndex === -1 ? "Add" : "Update" }}
+        </button>
+        <button type="button" @click="cancelEdit">Cancel</button>
       </form>
     </div>
 
     <div class="table-container">
-        <table>
+      <table>
         <thead>
-            <tr>
+          <tr>
             <th>Cover Art</th>
             <th>Name</th>
             <th>Artist</th>
             <th>Genre</th>
             <th>Duration</th>
-            <th> </th>
-            </tr>
+            <th></th>
+          </tr>
         </thead>
         <tbody>
-            <tr v-for="song in filteredSongs" :key="song.id">
+          <tr v-for="song in filteredSongs" :key="song.id">
             <td>
-                <img :src="song.coverArt" alt="Cover Art" height="50" />
+              <img :src="song.coverArt" alt="Cover Art" height="50" />
             </td>
-            <td>{{ song.name }}</td>
+            <td>{{ song.title }}</td>
             <td>{{ song.artist }}</td>
             <td>{{ song.genre }}</td>
             <td>{{ song.duration }}</td>
@@ -49,215 +53,51 @@
               <button @click="editSong(song)">Edit</button>
               <button @click="deleteSong(song.id)">Delete</button>
             </td>
-            </tr>
+          </tr>
         </tbody>
-        </table>
+      </table>
     </div>
   </div>
 </template>
 
 <script>
+import songAPI from "../src/services/SongService.js";
+
 export default {
-    name: "global-list",
+  name: "global-list",
   components: {},
 
   data() {
     return {
-      songs: [
-        {
-            id: 1,
-            coverArt: "URL_TO_COVER_ART_IMAGE",
-            name: "Dancing in the Moonlight",
-            artist: "Eleanor Summers",
-            genre: "Pop",
-            duration: "3:45",
-            
-        },
-        {
-            id: 2,
-            coverArt: "URL_TO_COVER_ART_IMAGE",
-            name: "Rolling Thunder",
-            artist: "Aiden Miller",
-            genre: "Rock",
-            duration: "4:12",
-            
-        },
-        {
-            id: 3,
-            coverArt: "URL_TO_COVER_ART_IMAGE",
-            name: "Electric Dreams",
-            artist: "Olivia Walker",
-            genre: "Electronic",
-            duration: "5:20",
-            
-        },
-        {
-            id: 4,
-            coverArt: "URL_TO_COVER_ART_IMAGE",
-            name: "Midnight Serenade",
-            artist: "Lucas Foster",
-            genre: "Jazz",
-            duration: "3:58",
-            
-        },
-        {
-            id: 5,
-            coverArt: "URL_TO_COVER_ART_IMAGE",
-            name: "Sunny Side Up",
-            artist: "Isabella Knight",
-            genre: "Funk",
-            duration: "4:30",
-            
-        },
-        {
-            id: 6,
-            coverArt: "URL_TO_COVER_ART_IMAGE",
-            name: "Lost in Paradise",
-            artist: "Gabriel Smith",
-            genre: "Indie",
-            duration: "3:15",
-            
-        },
-        {
-            id: 7,
-            coverArt: "URL_TO_COVER_ART_IMAGE",
-            name: "Rhythm of the Rain",
-            artist: "Sophia Carter",
-            genre: "Country",
-            duration: "4:05",
-            
-        },
-        {
-            id: 8,
-            coverArt: "URL_TO_COVER_ART_IMAGE",
-            name: "Echoes of Eternity",
-            artist: "Nathan Hill",
-            genre: "Ambient",
-            duration: "6:18",
-            
-        },
-        {
-            id: 9,
-            coverArt: "URL_TO_COVER_ART_IMAGE",
-            name: "Urban Odyssey",
-            artist: "Zoe Rivera",
-            genre: "Hip Hop",
-            duration: "3:50",
-            
-        },
-        {
-            id: 10,
-            coverArt: "URL_TO_COVER_ART_IMAGE",
-            name: "Sunset Melodies",
-            artist: "Liam Mitchell",
-            genre: "Chill",
-            duration: "4:30",
-            
-        },
-        {
-            id: 11,
-            coverArt: "URL_TO_COVER_ART_IMAGE",
-            name: "Whispers in the Wind",
-            artist: "Mason Reed",
-            genre: "Acoustic",
-            duration: "3:28",
-            
-        },
-        {
-            id: 12,
-            coverArt: "URL_TO_COVER_ART_IMAGE",
-            name: "Starry Night Serenade",
-            artist: "Aria Hughes",
-            genre: "Classical",
-            duration: "2:56",
-            
-        },
-        {
-            id: 13,
-            coverArt: "URL_TO_COVER_ART_IMAGE",
-            name: "City Lights",
-            artist: "Evan Cooper",
-            genre: "Pop",
-            duration: "3:42",
-            
-        },
-        {
-            id: 14,
-            coverArt: "URL_TO_COVER_ART_IMAGE",
-            name: "Euphoric Echoes",
-            artist: "Zara Turner",
-            genre: "Electronic",
-            duration: "4:10",
-            
-        },
-        {
-            id: 15,
-            coverArt: "URL_TO_COVER_ART_IMAGE",
-            name: "Mystic Voyage",
-            artist: "Leo Ramirez",
-            genre: "World",
-            duration: "5:15",
-            
-        },
-        {
-            id: 16,
-            coverArt: "URL_TO_COVER_ART_IMAGE",
-            name: "Sunrise Serenity",
-            artist: "Ava Foster",
-            genre: "Chill",
-            duration: "3:20",
-            
-        },
-        {
-            id: 17,
-            coverArt: "URL_TO_COVER_ART_IMAGE",
-            name: "Radiant Hearts",
-            artist: "Owen Patel",
-            genre: "Indie",
-            duration: "4:05",
-            
-        },
-        {
-            id: 18,
-            coverArt: "URL_TO_COVER_ART_IMAGE",
-            name: "Wanderlust",
-            artist: "Luna Bennett",
-            genre: "Folk",
-            duration: "3:48",
-            
-        },
-        {
-            id: 19,
-            coverArt: "URL_TO_COVER_ART_IMAGE",
-            name: "Eternal Embrace",
-            artist: "Nora Parker",
-            genre: "Ambient",
-            duration: "6:30",
-            
-        },
-        {
-            id: 20,
-            coverArt: "URL_TO_COVER_ART_IMAGE",
-            name: "Celestial Dreams",
-            artist: "Ella Baker",
-            genre: "Electronic",
-            duration: "5:10",
-            
-        }
-        
-      ],
-      searchQuery: '',
+      song: {
+        title: "",
+        artist: "",
+        genre: "",
+        duration: "",
+        songURL: "",
+        coverArt: "",
+      },
+      songs: [],
+      searchQuery: "",
       showAddForm: false,
       newSong: {
-        name: '',
-        artist: '',
-        genre: '',
-        duration: '',
-        coverArt: '',
+        title: "",
+        artist: "",
+        genre: "",
+        duration: "",
+        songURL: "",
+        coverArt: "",
       },
-      editingSong: null,
+      editingSong: {},
       editingSongIndex: -1,
     };
+  },
+  created() {
+    songAPI.list().then((response) => {
+      console.log(response.data);
+      this.songs = response.data;
+      console.log(this.songs);
+    });
   },
   computed: {
     filteredSongs() {
@@ -266,111 +106,116 @@ export default {
       }
       const query = this.searchQuery.toLowerCase();
       return this.songs.filter(
-        song =>
-          song.name.toLowerCase().includes(query) ||
+        (song) =>
+          song.title.toLowerCase().includes(query) ||
           song.artist.toLowerCase().includes(query) ||
           song.genre.toLowerCase().includes(query) ||
           song.duration.toLowerCase().includes(query)
       );
-    }
+    },
   },
   methods: {
+    showForm() {
+      this.showAddForm = true;
+    },
+    hideForm() {
+      this.showAddForm = false;
+    },
     addNewSong() {
-        if (this.editingSongIndex === -1) {
-            this.songs.push({
-                id: this.songs.length + 1,
-                name: this.editingSong.name,
-                artist: this.editingSong.artist,
-                genre: this.editingSong.genre,
-                duration: this.editingSong.duration,
-                coverArt: this.editingSong.coverArt,
-            });
-        } else {
-            this.songs[this.editingSongIndex] = { ...this.editingSong };
-        }
+      if (this.editingSongIndex === -1) {
+        this.songs.push({
+          id: this.songs.length + 1,
+          title: this.editingSong.title,
+          artist: this.editingSong.artist,
+          genre: this.editingSong.genre,
+          duration: this.editingSong.duration,
+          coverArt: this.editingSong.coverArt,
+        });
+      } else {
+        this.songs[this.editingSongIndex] = { ...this.editingSong };
+      }
 
-        this.editingSong = {
-            name: '',
-            artist: '',
-            genre: '',
-            duration: '',
-            coverArt: '',
-        };
-        this.editingSongIndex = -1;
-        this.showAddForm = false;
+      this.editingSong = {
+        title: "",
+        artist: "",
+        genre: "",
+        duration: "",
+        coverArt: "",
+      };
+      this.editingSongIndex = -1;
+      this.hideForm();
     },
 
     cancelEdit() {
-        this.editingSong = {
-            name: '',
-            artist: '',
-            genre: '',
-            duration: '',
-            coverArt: '',
-        };
-        this.editingSongIndex = -1;
-        this.showAddForm = false;
+      this.editingSong = {
+        title: "",
+        artist: "",
+        genre: "",
+        duration: "",
+        coverArt: "",
+      };
+      this.editingSongIndex = -1;
+      this.hideForm();
     },
     editSong(song) {
-        this.editingSong = { ...song };
-        this.editingSongIndex = this.songs.findIndex(s => s.id === song.id);
-        this.showAddForm = true;
+      this.editingSong = { ...song };
+      this.editingSongIndex = this.songs.findIndex((s) => s.id === song.id);
+      this.showForm();
     },
-    
+
     deleteSong(songId) {
-        const index = this.songs.findIndex(s => s.id === songId);
-        if (index !== -1) {
-            this.songs.splice(index, 1);
-        }
-    }
-  }
+      const index = this.songs.findIndex((s) => s.id === songId);
+      if (index !== -1) {
+        this.songs.splice(index, 1);
+      }
+    },
+  },
 };
 </script>
 
 <style>
 body {
-    background-color: rgb(50, 50, 50);
+  background-color: rgb(50, 50, 50);
 }
 
 h2 {
-    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-    text-align: center;
-    color: silver;
-    background-color: rgb(4, 92, 4);
-    padding: 20px;
-    border-radius: 10px;
-    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
-    max-width: 800px;
-    margin-bottom: 20px;
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+  text-align: center;
+  color: silver;
+  background-color: rgb(4, 92, 4);
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+  max-width: 800px;
+  margin-bottom: 20px;
 }
 
 .input {
-    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-    text-align: center;
-    max-width: 800px;
-    margin: 0 auto;
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+  text-align: center;
+  max-width: 800px;
+  margin: 0 auto;
 }
 
 table {
-    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-    color: silver;
-    background-color: rgb(100, 100, 100);
-    padding: 20px;
-    border-radius: 10px;
-    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
-    max-width: 800px;
-    margin: 0 auto;
-    border-collapse: collapse;
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+  color: silver;
+  background-color: rgb(100, 100, 100);
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+  max-width: 800px;
+  margin: 0 auto;
+  border-collapse: collapse;
 }
 
 th {
-    padding: 10px;
-    
+  padding: 10px;
+  text-align: left;
 }
 
 td {
-    padding: 10px;
-    
+  padding: 10px;
 }
 
 .table-container {
@@ -378,7 +223,6 @@ td {
   overflow-y: auto;
   margin-top: 20px;
   position: relative;
-  
 }
 
 table thead {
@@ -386,7 +230,7 @@ table thead {
   top: 0;
   background-color: rgb(4, 92, 4);
   border-radius: 10px;
-  box-shadow: 0px 0px 10px  rgb(4, 92, 4);
+  box-shadow: 0px 0px 10px rgb(4, 92, 4);
   z-index: 1;
 }
 </style>
--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -7,7 +7,13 @@
       </div>
       <div class="form-input-group">
         <label for="username">Username</label>
-        <input type="text" id="username" v-model="user.username" required autofocus />
+        <input
+          type="text"
+          id="username"
+          v-model="user.username"
+          required
+          autofocus
+        />
       </div>
       <div class="form-input-group">
         <label for="password">Password</label>
@@ -15,44 +21,53 @@
       </div>
       <div class="form-input-group">
         <label for="confirmPassword">Confirm Password</label>
-        <input type="password" id="confirmPassword" v-model="user.confirmPassword" required />
+        <input
+          type="password"
+          id="confirmPassword"
+          v-model="user.confirmPassword"
+          required
+        />
       </div>
       <button type="submit">Create Account</button>
-      <p><router-link class="login-link" :to="{ name: 'login' }">Already have an account? Log in.</router-link></p>
+      <p>
+        <router-link class="login-link" :to="{ name: 'login' }"
+          >Already have an account? Log in.</router-link
+        >
+      </p>
     </form>
   </div>
 </template>
 
 <script>
-import authService from '../services/AuthService';
+import authService from "../services/AuthService";
 
 export default {
-  name: 'register',
+  name: "register",
   data() {
     return {
       user: {
-        username: '',
-        password: '',
-        confirmPassword: '',
-        role: 'user',
+        username: "",
+        password: "",
+        confirmPassword: "",
+        role: "user",
       },
       registrationErrors: false,
-      registrationErrorMsg: 'There were problems registering this user.',
+      registrationErrorMsg: "There were problems registering this user.",
     };
   },
   methods: {
     register() {
       if (this.user.password != this.user.confirmPassword) {
         this.registrationErrors = true;
-        this.registrationErrorMsg = 'Password & Confirm Password do not match.';
+        this.registrationErrorMsg = "Password & Confirm Password do not match.";
       } else {
         authService
           .register(this.user)
           .then((response) => {
             if (response.status == 201) {
               this.$router.push({
-                path: '/login',
-                query: { registration: 'success' },
+                path: "/login",
+                query: { registration: "success" },
               });
             }
           })
@@ -60,14 +75,14 @@ export default {
             const response = error.response;
             this.registrationErrors = true;
             if (response.status === 400) {
-              this.registrationErrorMsg = 'Bad Request: Validation Errors';
+              this.registrationErrorMsg = "Bad Request: Validation Errors";
             }
           });
       }
     },
     clearErrors() {
       this.registrationErrors = false;
-      this.registrationErrorMsg = 'There were problems registering this user.';
+      this.registrationErrorMsg = "There were problems registering this user.";
     },
   },
 };
@@ -81,19 +96,21 @@ label {
   margin-right: 0.5rem;
 }
 form {
-   font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-    text-align: center;
-    color: #DC9D00;
-    background-color: rgb(100, 100, 100);
-    padding: 20px;
-    border-radius: 10px;
-    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
-    max-width: 800px;
-    margin-bottom: 20px;
-  
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+  text-align: center;
+  color: #dc9d00;
+  background-color: rgb(100, 100, 100);
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+  max-width: 800px;
+  margin-bottom: 20px;
 }
 .login-link {
-   color: #DC9D00;
+  color: #dc9d00;
   background-color: rgb(100, 100, 100);
+}
+h2 {
+  margin: 0 auto;
 }
 </style>
