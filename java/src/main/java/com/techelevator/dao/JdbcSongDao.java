8c8410f87115f27a3660c90a1604f337cc6b531c
Brody Gallaher <tersesoliloquist@gmail.com>
2023-08-09 10:06:44 -0400

    br

--- java/src/main/java/com/techelevator/dao/JdbcSongDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcSongDao.java
@@ -71,19 +71,21 @@ public class JdbcSongDao implements SongDao {
     }
 
     @Override
-    public Song getSongByTitle(String title) {
+    public List<Song> getAllSongsByTitle(String title) {
         Song song = null;
+        List<Song> songs = new ArrayList<>();
         String sql = "SELECT * FROM \"song\" " + "WHERE title = ?";
 
         try {
             SqlRowSet result = jdbcTemplate.queryForRowSet(sql, title);
             if (result.next()) {
                 song = mapRowToSong(result);
+                songs.add(song);
             }
         } catch (CannotGetJdbcConnectionException e) {
             throw new DaoException("Unable to connect to server or database", e);
         }
-        return song;
+        return songs;
     }
     @Override
     @ResponseStatus(HttpStatus.CREATED)
@@ -101,9 +103,25 @@ public class JdbcSongDao implements SongDao {
     }
 
     @Override
-    public int deleteSongById(int songID) {
-        return 0;
+    public Song updateSong(Song song) {
+
+
+
+
+
+        return null;
     }
+
+//    @Override
+//    public int deleteSongById(int songID) {
+//
+//        String sql = "DELETE FROM \"song\" " + "WHERE song_id = ?;";
+//
+//        try{
+//            jdbcTemplate.
+//        }
+//        return 0;
+//    }
 
     private Song mapRowToSong(SqlRowSet rowSet) {
         Song song = new Song();
